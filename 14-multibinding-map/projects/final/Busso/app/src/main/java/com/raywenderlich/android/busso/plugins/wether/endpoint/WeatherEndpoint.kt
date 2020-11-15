815b616d30d537c68d3067493c3ef73e174092ec
Massimo Carli <massimocarli@Massimos-MBP.cust.communityfibre.co.uk>
2020-11-15 15:44:08 +0000

    Starter and Final project for chapter 14

---
+++ 14-multibinding-map/projects/final/Busso/app/src/main/java/com/raywenderlich/android/busso/plugins/wether/endpoint/WeatherEndpoint.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.busso.plugins.wether.endpoint
+
+import com.raywenderlich.android.busso.conf.BUSSO_SERVER_BASE_URL
+import com.raywenderlich.android.busso.plugins.api.InformationEndpoint
+import com.raywenderlich.android.busso.plugins.model.InfoMessage
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface WeatherEndpoint : InformationEndpoint {
+  @GET("${BUSSO_SERVER_BASE_URL}weather/{lat}/{lng}")
+  override fun fetchInformation(
+      @Path("lat") latitude: Double,
+      @Path("lng") longitude: Double
+  ): Single<InfoMessage>
+}
